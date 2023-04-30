8ebda6dac340a5057fb0c85cb3f09ab06766008b
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-05-01 01:32:01 +0900

    chatGPT가 준 응답 데이터 전처리 완료

--- app/ChatGPT.java
+++ app/ChatGPT.java
@@ -6,37 +6,12 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-//import java.util.Stack;
-//import json;
 
-//import org.json.JSONArray;
-//import org.json.JSONObject;
 public class Main {
-//    public Main(String key) throws IOException {
-//        URL url = new URL("https://api.openai.com/v1/completions");
-//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//        con.setRequestMethod("POST");
-//        con.setRequestProperty("Content-Type", "application/json; utf-8");
-//        con.setRequestProperty("Authorization", "Bearer " + key);
-//        con.setDoOutput(true);
-//        String prompt = "";
-////        JSONObject jsonObject = new JSONObject();
-////        jsonObject.put("key1", "value1");
-////        jsonObject.put("key2", "value2");
-////        String input = "{\"model\": \"text-davinci-003\", \"prompt\": \""+prompt+"\",\"max_tokens\":2048}";
-//        JSONObject input = new JSONObject();
-//        input.put("model", "text-davinci-003");
-//        input.put("prompt", "");
-//        input.put("max_tokens", 2048);
-//        // 입력 데이터의 디버그 출력
-//        System.out.println(input.toString());
-
-//    }
-//    public String MainAPI(String prompt){
-//
-//    }
     public static String callAPI(String key, String prompt) {
         try {
             URL url = new URL("https://api.openai.com/v1/completions");
@@ -45,10 +20,7 @@ public class Main {
             con.setRequestProperty("Content-Type", "application/json; utf-8");
             con.setRequestProperty("Authorization", "Bearer " + key );
             con.setDoOutput(true);
-//            String input = "{\"model\": \"text-davinci-003\", \"prompt\": \""+prompt+"\",\"max_tokens\":2048}";
-            // 요청 매개변수로 JSON 문자열 생성
-//            String input = "{\"prompt\": \"" + prompt + "\", \"temperature\":0.5, \"max_tokens\":1024, \"top_p\":1,\"frequency_penalty\":0, \"presence_penalty\":0 }";
-            // 입력 데이터의 디버그 출력
+
             JSONObject input = new JSONObject();
             input.put("model", "text-davinci-003");
             input.put("prompt", prompt);
@@ -86,8 +58,15 @@ public class Main {
                 Object obj = parser.parse(response.toString());
                 // Object 데이터를 JSONObject로 변환
                 JSONObject jsonResponse = (JSONObject) obj;
-//                System.out.println(obj);
-                return obj.toString();
+                //jsonResponse에서 text 부분만 추출하기 위한 과정
+                ////
+                JSONArray choices = (JSONArray) jsonResponse.get("choices");
+                // choices 부분 추출하여 [] 배열 벗기기
+                JSONObject choice = (JSONObject) choices.get(0);
+                // text 필드 추출
+                String text = (String) choice.get("text");
+                return text;
+//                return jsonResponse.toString(); 응답받은 json데이터 자체
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -102,6 +81,5 @@ public class Main {
         String prompt = scanner.nextLine();
         String result_api = callAPI("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", prompt);
         System.out.println(result_api);
-
     }
 }
