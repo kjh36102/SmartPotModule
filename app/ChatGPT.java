0e3bddfd84151fc8d2d5a1071412081241a1f9f5
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-05-05 17:15:57 +0900

    feedback와 recommand 분리, 질문 prompt 정형화

--- app/ChatGPT.java
+++ app/ChatGPT.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.example.mysampleservice;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -8,32 +8,42 @@ import java.net.URL;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
 public class ChatGPT {
     URL url;
     HttpURLConnection con;
     JSONObject input;
+    String key;
     public ChatGPT(String key){
         try {
             url = new URL("https://api.openai.com/v1/completions");
-            con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", "application/json; utf-8");
-            con.setRequestProperty("Authorization", "Bearer " + key);
-            con.setDoOutput(true);
-
             input = new JSONObject();
             input.put("model", "text-davinci-003");
             input.put("prompt", "");
             input.put("max_tokens", 2048);
+            this.key = key;
         }catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public String Chat(String prompt) {
-        this.input.put("prompt", prompt);
-        // 연결의 출력 스트림에 입력 데이터 쓰기
+    public String feedback(String name, double temperature, double humidity, double nitrogen, double phosphorus, double potassium, double ph, double elect) {
+        this.input.put("prompt", "식물 "+name+"의 토양 온도는 "+temperature+"도, 토양 습도는 "+humidity+"%, 질소가 "+nitrogen+"mg/kg, 인이 "+phosphorus+"mg/kg, 칼륨이 "+potassium+" mg/kg, 토양 ph가 "+ph+" 전기 전도도는 "+elect+"us/cm, 광량은 6500lux이다. 문제가 있는 것을 분석해주고, 개선방안을 제안해줘.");
+        return process();
+    }
+    public String recommand(String name){
+        this.input.put("prompt", "식물 "+name+"의 추천 토양 온도(섭씨), 추천 토양 습도(%), 추천 N(mg/kg), 추천 P(mg/kg), 추천 K(mg/kg), 추천 토양산화도(ph), 추천 토양전기전도도(us/cm) 의 각 수치를 범위로 만들어서 아래처럼 json을 만들어줘\n" +
+                "{\"추천_토양온도\": {\"최소값\": xx,\"최대값\": xx,\"단위\": \"℃\"}, \"추천_토양습도\": {...}, \"추천_N\": {...}, \"추천_P\": {...}, \"추천_K\": {...}, \"추천_토양산화도\": {...}, \"추천_토양전기전도도\": {...} }");
+        return process();
+    }
+    public String process(){
         try {
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Authorization", "Bearer " + key);
+            con.setDoOutput(true);
+            System.out.println(input.toString());
+            // 연결의 출력 스트림에 입력 데이터 쓰기
+
             try (OutputStream os = con.getOutputStream()) {
                 String jsonRequest = input.toString();
                 byte[] inputBytes = jsonRequest.getBytes("utf-8");
@@ -57,18 +67,19 @@ public class ChatGPT {
                     //공백제거
                     response.append(responseLine.trim());
                 }
+                System.out.println(response.toString());
                 //String 데이터를 Object로 변환
                 JSONParser parser = new JSONParser();
                 Object obj = parser.parse(response.toString());
                 // Object 데이터를 JSONObject로 변환
                 JSONObject jsonResponse = (JSONObject) obj;
                 //jsonResponse에서 text 부분만 추출하기 위한 과정
-                ////
                 JSONArray choices = (JSONArray) jsonResponse.get("choices");
                 // choices 부분 추출하여 [] 배열 벗기기
                 JSONObject choice = (JSONObject) choices.get(0);
                 // text 필드 추출
                 String text = (String) choice.get("text");
+                con.disconnect();
                 return text;
             }
         } catch (Exception e){
