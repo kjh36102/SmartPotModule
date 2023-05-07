5f111d72786b75cfe48014b3b497bbd9de55026e
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-05-08 01:16:37 +0900

    함수 호출 방식 바뀜

--- app/MainActivity.java
+++ app/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ChatGPT chatGPT = new ChatGPT("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");
+        ChatGPT chatGPT = new ChatGPT();
         textView = findViewById(R.id.textView);
         textView2 = findViewById(R.id.textView2);
         Button button = findViewById(R.id.button);
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 //버튼 눌렀을 때 network를 사용하는 API인 경우 Thread로 처리해야 함
                 new Thread() {
                     public void run() {
-                        String feedbackResponse = chatGPT.feedback("해바라기", 24.1, 17.2, 13, 5, 69, 5, 249);
+                        String feedbackResponse = chatGPT.feedback("해바라기", 24.1, 17.2, 13, 5, 69, 5, 249, 6500);
                         //response를 받는 즉시 스마트폰 UI에 해당되는 값을 적용할 때 다른 Thread에서는 변경을 못해줌
                         //메인 쓰레드에서 변경을 할 수 있음
                         runOnUiThread(new Runnable() { //메인쓰레드에서 코드를 실행할 수 있도록 해주는 메서드
