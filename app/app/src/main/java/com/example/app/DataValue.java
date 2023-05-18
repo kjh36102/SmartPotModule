eaf21e2c4528eb505db90377ea855ab560d3dba4
sci1125 <starchang25@naver.com>
2023-05-18 18:41:50 +0900

    식물관리 최신

---
+++ app/app/src/main/java/com/example/app/DataValue.java
@@ -0,0 +1,40 @@
+package com.example.app;
+
+public class DataValue {
+    String date;
+    String time;
+    String value;
+
+    public DataValue() {
+    }
+
+    public DataValue(String date, String time, String value) {
+        this.date = date;
+        this.time = time;
+        this.value = value;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
