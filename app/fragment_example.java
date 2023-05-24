8e875be61a12fbd86460a54da24f07b04c14774e
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-05-24 22:09:09 +0900

    Update fragment_example.java

--- app/fragment_example.java
+++ app/fragment_example.java
@@ -14,17 +14,17 @@ import android.widget.TextView;
 
 import org.json.simple.JSONObject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment2#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Fragment2 extends Fragment {
     TextView textView;
     TextView textView2;
     TextView textView3;
+    TextView textView4;
+
     Button button;
     Button button2;
+    Button button3;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -36,19 +36,22 @@ public class Fragment2 extends Fragment {
         textView = view.findViewById(R.id.textView);
         textView2 = view.findViewById(R.id.textView2);
         textView3 = view.findViewById(R.id.textView3);
+        textView4 = view.findViewById(R.id.textView4);
+
         button = view.findViewById(R.id.button);
         button2 = view.findViewById(R.id.button2);
+        button3 = view.findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new Thread(){
                     public void run(){
-                        JSONObject recommandResponse = chatGPT.recommand("선인장");
+                        JSONObject scoreResponse = chatGPT.score("선인장", 20, 60, 120, 60, 100, 6, 400, 5000);
                         Handler handler = new Handler(Looper.getMainLooper());
                         handler.post(new Runnable() {
                             @Override
                             public void run() {
-                                textView.setText(recommandResponse.toJSONString());
+                                textView2.setText(scoreResponse.toJSONString());
                             }
                         });
                     }
@@ -59,12 +62,28 @@ public class Fragment2 extends Fragment {
             public void onClick(View v) {
                 new Thread() {
                     public void run () {
-                        JSONObject feedbackResponse = chatGPT.feedback("선인장", 20, 60, 120, 60, 100, 6, 400, 5000);
+                        String feedbackResponse = chatGPT.feedback("선인장", 20, 60, 120, 60, 100, 6, 400, 5000);
                         Handler handler = new Handler(Looper.getMainLooper());
                         handler.post(new Runnable() {
                             @Override
                             public void run() {
-                                textView2.setText(feedbackResponse.toJSONString());
+                                textView3.setText(feedbackResponse);
+                            }
+                        });
+                    }
+                }.start();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new Thread() {
+                    public void run () {
+                        String tipResponse = chatGPT.tips("볼부 선인장");
+                        Handler handler = new Handler(Looper.getMainLooper());
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView4.setText(tipResponse);
                             }
                         });
                     }
